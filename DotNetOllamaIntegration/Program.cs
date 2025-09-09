7748452e90f621edd87e78a3ab2ebb36d92492a8
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-09-08 21:24:44 -0300

    feat: Adding the ollama integration

---
+++ DotNetOllamaIntegration/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using OllamaSharp;
+
+var url = new Uri("http://localhost:11434");
+var client = new OllamaApiClient(url);
+var models = await client.ListLocalModelsAsync();
+
+//foreach(var model in models)
+//{
+//    Console.WriteLine(model.Name);
+//}
+
+//await client.PullModelAsync("phi3:latest");
+
+client.SelectedModel = "phi3:latest";
+
+var chat = new Chat(client);
+var prompt = Console.ReadLine() ?? String.Empty;
+
+await foreach (var answer in chat.SendAsync(prompt))
+{
+    Console.Write(answer);
+}
+
+
+Console.WriteLine("--");
